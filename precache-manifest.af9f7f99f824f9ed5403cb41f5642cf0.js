958b62ab68f62eddbd944df9e9c34b15553245cb
Aditya Bhamidi <adityabhamidi@hotmail.com>
2020-09-16 19:14:35 +0200

    Updates

---
+++ precache-manifest.af9f7f99f824f9ed5403cb41f5642cf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c545c69adb30b56109bc3fa5a85d6fb6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c0111473bb169d3549d6",
+    "url": "/monsters-rolodex/static/css/main.c5c5a8b3.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0111473bb169d3549d6",
+    "url": "/monsters-rolodex/static/js/main.91c69a78.chunk.js"
+  },
+  {
+    "revision": "dfba82531f669c6858b0",
+    "url": "/monsters-rolodex/static/js/runtime-main.589ddcef.js"
+  }
+]);
